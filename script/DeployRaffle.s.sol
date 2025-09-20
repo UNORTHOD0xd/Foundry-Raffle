3bc54fa753fc75d628651965a4625b90cbba2e19
Dorien Bourne <dorienjoshuabourne@gmail.com>
2025-09-19 21:19:12 -0500

    Added consumer programmatically

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,10 +4,12 @@ pragma solidity 0.8.19;
 import { Script } from "forge-std/Script.sol";
 import { Raffle } from "../src/Raffle.sol";
 import { HelperConfig } from "./HelperConfig.s.sol";
-import { CreateSubscription } from "./Interactions.s.sol";
+import { CreateSubscription, FundSubscription, AddConsumer } from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() public {}
+    function run() public {
+        deployContract();
+    }
 
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
@@ -17,7 +19,12 @@ contract DeployRaffle is Script {
         if (config.subscriptionId == 0) {
             // create subscriptionId logic
             CreateSubscription createSubscription = new CreateSubscription(); // Instantiate the CreateSubscription contract
-            (config.subscriptionId, config.vrfCoordinatorV2_5) = createSubscription.createSubscription(config.vrfCoordinatorV2_5, config.account); 
+            (config.subscriptionId, config.vrfCoordinator) = 
+            createSubscription.createSubscription(config.vrfCoordinator); 
+
+            // fund subscription logic
+            FundSubscription fundSubscription = new FundSubscription(); // Instantiate the FundSubscription contract
+            fundSubscription.fundSubscription(config.vrfCoordinator, config.subscriptionId, config.link);
         }
 
         vm.startBroadcast();
@@ -30,6 +37,11 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        // dont need to broadcast ...
+        addConsumer.addConsumer(address(raffle), config.vrfCoordinator, config.subscriptionId);
+
         return (raffle, helperConfig);
     }
 }
